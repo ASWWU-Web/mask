638d5514dd3e80cf3d801a9f26533c92b8a43ad8
kadensu <ksukachevin@gmail.com>
2019-03-16 21:34:28 -0700

    Modal closes with back button

--- src/app/shared/profile-modal/profile-modal.component.ts
+++ src/app/shared/profile-modal/profile-modal.component.ts
@@ -4,8 +4,8 @@ import { FormGroup, FormArray, Validators } from '@angular/forms';
 import { CURRENT_YEAR} from '../../config';
 import { ProfileModel } from '../profile.model';
 import { RequestService } from '../../RequestService/request.service';
-import { Subscription } from 'rxjs';
 import { ActivatedRoute} from '@angular/router';
+import { PlatformLocation } from '@angular/common';
 import { ProfileFullComponent } from '../profile-full/profile-full.component';
 
 @Component({
@@ -18,20 +18,15 @@ export class ProfileModalContentComponent implements OnInit {
   @Input() username: String;
   @Input() year: String;
   profile: ProfileModel;
-  private subscription: Subscription;
-  ballotForm: FormGroup;
-  clostFormFlag = false;
 
   constructor(public activeModal: NgbActiveModal, private rs: RequestService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.year = this.year ? this.year : CURRENT_YEAR;
-    this.rs.get('/profile/' + this.year + '/' + this.username, (data) => this.profile = new ProfileModel(data), undefined);
-  }
-
-  onCloseModal(): void {
-    // set the close form flag for when the votes have been submitted
-    this.clostFormFlag = true;
+    const url = '/profile/' + this.year + '/' + this.username;
+    this.rs.get(url, (data) => this.profile = new ProfileModel(data), undefined);
+    const stateObj = { hello: 'there '};
+    history.pushState(stateObj, 'Profile View', url);
   }
 }
 
@@ -43,11 +38,19 @@ export class ProfileModalContentComponent implements OnInit {
 })
 export class ProfileModalComponent implements OnInit {
   modal: NgbModalRef;
+  backButton: Boolean = false;
 
-  constructor(private modalService: NgbModal) { }
-
-  ngOnInit() {
+  constructor(private modalService: NgbModal, private location: PlatformLocation) {
+    // Closes modal if back button is clicked
+    location.onPopState(() => {
+      if (this.backButton) {
+        this.modal.close();
+        this.backButton = false;
+      }
+    });
   }
+
+  ngOnInit() {}
 
   open(username, year): void {
     // save the modal reference so we can close it
@@ -55,5 +58,13 @@ export class ProfileModalComponent implements OnInit {
     // pass data to the modal inputs
     this.modal.componentInstance.username = username;
     this.modal.componentInstance.year = year;
+    // Flag variable will be true for location.onPopState if the back button was clicked; else will be set to false first
+    this.backButton = true;
+
+    // Promise is rejected if click outside of modal or exit button is clicked; resolved if back button is clicked
+    this.modal.result.then(() => {}, () => {
+      this.backButton = false;
+      history.back();
+    });
   }
 }
