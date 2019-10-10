76b2efde4aeb2f4ba2b0497d96387491359e0e9e
morini <nicholas.morin@wallawalla.edu>
2019-10-09 17:47:41 -0700

    updated year to 1920

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,14 +1,14 @@
-//IMAGES
+// IMAGES
 export const MEDIA_URI = 'https://aswwu.com/media';
-export const MEDIA_XS = MEDIA_URI + "/img-xs";
-export const MEDIA_SM = MEDIA_URI + "/img-sm";
-export const MEDIA_MD = MEDIA_URI + "/img-md";
-export const MEDIA_LG = MEDIA_URI + "/img-lg";
+export const MEDIA_XS = MEDIA_URI + '/img-xs';
+export const MEDIA_SM = MEDIA_URI + '/img-sm';
+export const MEDIA_MD = MEDIA_URI + '/img-md';
+export const MEDIA_LG = MEDIA_URI + '/img-lg';
 export const DEFAULT_PHOTO = 'images/default_mask/default.jpg';
 
-//YEARS
-export const CURRENT_YEAR = "1819";
-export const ARCHIVE_YEARS = ["1718", "1617","1516","1415","1314","1213","1112","1011","0910","0809","0708","0607"];
+// YEARS
+export const CURRENT_YEAR = '1920';
+export const ARCHIVE_YEARS = ['1819', '1718', '1617','1516','1415','1314','1213','1112','1011','0910','0809','0708','0607'];
 
-//AUTH
-export const AUTH_URI = "https://saml.aswwu.com/";
+// AUTH
+export const AUTH_URI = 'https://saml.aswwu.com/';
